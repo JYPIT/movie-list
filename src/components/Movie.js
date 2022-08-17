f75c7cf8142ecd6ccbb21fa5f71f6ad4572cfdf7
JYPIT <jypit@naver.com>
2022-08-17 09:47:04 +0900

    css 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
+
 const imageURL = "https://image.tmdb.org/t/p/w300";
 
 function Movie({ id, poster_path, title, rating, release_date, overview }) {
   return (
-    <div>
-      <img src={imageURL + poster_path} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link> ⭐{rating}
-      </h2>
-      <h3>{release_date}</h3>
-      <p>{overview}</p>
+    <div className={styles.movie}>
+      <img src={imageURL + poster_path} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link> ⭐{rating}
+        </h2>
+        <h3 className={styles.movie__release}>{release_date}</h3>
+        <p>{overview.length > 235 ? `${overview.slice(0, 235)}...` : overview}</p>
+      </div>
     </div>
   );
 }
